ec6fa99411a931939b5745b8e7b1c59d9a54ae06
AndrisKostiks <kostiksandriss@gmail.com>
2020-12-05 00:51:11 +0200

    V-0: Base Init With Base Components

--- apps/vehicols-mono/src/app/app.module.ts
+++ apps/vehicols-mono/src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SearchComponent } from './components/search/search.component';
+import { UserComponent } from './components/user/user.component';
+import { DetailedSearchComponent } from './components/detailed-search/detailed-search.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PreFooterComponent } from './components/pre-footer/pre-footer.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [AppComponent, NavbarComponent, SearchComponent, UserComponent, DetailedSearchComponent, FooterComponent, PreFooterComponent],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
+
 })
 export class AppModule {}
---
+++ apps/vehicols-mono/src/app/components/detailed-search/detailed-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'vehicols-detailed-search',
+  templateUrl: './detailed-search.component.html',
+  styleUrls: ['./detailed-search.component.scss']
+})
+export class DetailedSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
