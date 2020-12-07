830dffeff92fae5678c24471777a851ac687fd7a
AndrisKostiks <kostiksandriss@gmail.com>
2020-12-07 21:36:09 +0200

    V-2: User navbar + footer + prefooter ready

--- apps/vehicols-mono/src/app/app-routing.module.ts
+++ apps/vehicols-mono/src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {FooterComponent} from "./components/footer/footer.component";
+import {HomeComponent} from "./components/home/home.component";
 
 const routes: Routes = [
-  {path: 'fuck', component: FooterComponent}
+  {path: '', component: HomeComponent}
 ];
 
 @NgModule({
--- apps/vehicols-mono/src/app/app.module.ts
+++ apps/vehicols-mono/src/app/app.module.ts
@@ -10,9 +10,12 @@ import { UserComponent } from './components/user/user.component';
 import { DetailedSearchComponent } from './components/detailed-search/detailed-search.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { PreFooterComponent } from './components/pre-footer/pre-footer.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { SearchFormComponent } from './components/search-form/search-form.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, SearchComponent, UserComponent, DetailedSearchComponent, FooterComponent, PreFooterComponent],
+  declarations: [AppComponent, NavbarComponent, SearchComponent, UserComponent, DetailedSearchComponent, FooterComponent, PreFooterComponent, SearchBarComponent, SearchFormComponent, HomeComponent],
   imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
