baca04c850076c60e955f42776c70b67a1b6caf7
Biotualetnij <66161409+1w3q2e4t6r5y@users.noreply.github.com>
2020-12-21 01:49:58 +0200

    MASREr:sell changes

--- apps/vehiclos-mono/src/app/components/Catalogue/sell-conf/sell-conf.component.ts
+++ apps/vehiclos-mono/src/app/components/Catalogue/sell-conf/sell-conf.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SellConfComponent implements OnInit {
 
+  a = [{route: 'suka'}, {route: 'blyat'}];
   constructor() { }
 
   ngOnInit(): void {
