cc631330808504aaa662551b341f72d4fedd8f70
Biotualetnij <66161409+1w3q2e4t6r5y@users.noreply.github.com>
2021-01-04 21:48:27 +0200

    MASTER: wishlist module complete

--- apps/vehiclos-mono/src/app/app-routing.module.ts
+++ apps/vehiclos-mono/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { SellComponent } from './components/Catalogue/sell/sell.component';
 import {DetailedSearchComponent} from "./components/Detailed-Search/detailed-search/detailed-search.component";
 import { ProductComponent } from './components/Catalogue/product/product.component';
 import { WishlistComponent } from './components/wishlist/wishlist.component';
+import { UserComponent } from './components/user/user.component';
 
 
 const routes: Routes = [
@@ -16,7 +17,7 @@ const routes: Routes = [
   {path: 'about-us', component: AboutUsComponent},
   {path: 'detailed-search', component: DetailedSearchComponent},
   {path: 'vehiclos-product', component:ProductComponent},
-  {path: 'vehiclos-wishlist', component:WishlistComponent},
+  {path: 'vehiclos-wishlist', component:UserComponent},
 ];
 
 @NgModule({
--- apps/vehiclos-mono/src/app/components/user/user.component.ts
+++ apps/vehiclos-mono/src/app/components/user/user.component.ts
@@ -6,7 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
-
+  style:string;
+  visible=true;
+  
+  StyleChanger(myCock){
+    
+    if(myCock==true){
+      this.style='right:0%;'
+    }else{
+      this.style='right:-100%'
+    }
+  }
+  
   constructor() { }
 
   ngOnInit(): void {
